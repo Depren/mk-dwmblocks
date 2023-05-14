71651c30c83f172edbb46f827798a94ed0f9423d
Marcus Kolber <marcus.kolber@gmail.com>
2023-05-14 08:54:49 -0400

    Functional version of Luke Smith's dwmblocks

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static const Block blocks[] = {
 	/*{"",	"sb-nettraf",	1,	16},*/
 	/*{"",	"sb-volume",	0,	10},*/
 	/*{"",	"sb-battery",	5,	3},*/
-	{"",	"sb-clock",	    60,	1},
+	{"",	"clock",	    60,	1},
 	/*{"",	"sb-internet",	5,	4},*/
 	/* {"",	"sb-iplocate", 0,	27}, */
 	/*{"",	"sb-help-icon",	0,	15},*/
